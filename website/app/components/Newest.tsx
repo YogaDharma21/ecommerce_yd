5bdbd0c57d399e28d810d2a5534dd183ea32a187
YogaDharma21 <yogacode2006@gmail.com>
2024-06-08 20:49:58 +0800

    fix: revert git commit

--- website/app/components/Newest.tsx
+++ website/app/components/Newest.tsx
@@ -24,7 +24,7 @@ export default async function Newest() {
           </h2>
           <Link
             href={"/all"}
-            className="text-[#2563eb] flex items-center gap-x-1 font-bold"
+            className="text-[#2563eb] flex items-center gap-x-1"
           >
             See All
             <span>
@@ -50,14 +50,14 @@ export default async function Newest() {
               </div>
               <div className="mt-4 flex justify-between">
                 <div>
-                  <h3 className="text-md text-gray-700 font-bold">
+                  <h3 className="text-sm text-gray-700 font-semibold">
                     {product.name}
                   </h3>
-                  <p className="mt-1 text-md text-gray-500 font-semibold">
+                  <p className="mt-1 text-sm text-gray-500">
                     {product.categoryName}
                   </p>
                 </div>
-                <p className="text-sm font-bold text-gray-900">
+                <p className="text-sm font-medium text-gray-900">
                   ${product.price}
                 </p>
               </div>
